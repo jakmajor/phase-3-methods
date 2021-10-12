a576efd38bea650ca33c3100d2f44330b0d68bae
Jak Major <66277064+jakmajor@users.noreply.github.com>
2021-10-12 14:25:51 -0700

    finished

--- methods.rb
+++ methods.rb
@@ -1,1 +1,24 @@
-# Your code here!
+def greet_programmer 
+    puts "Hello, programmer!"
+end
+
+def greet(name) 
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+def add(num1, num2)
+    sum = num1 + num2
+    sum
+end
+
+def halve(number)
+    if number.class == String 
+        return nil 
+    end 
+    math = number / 2 
+    math
+end
